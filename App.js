c642cde9944fee286b86e02a1100cc5bcbe799e5
vikrampurohit15 <139479132+vikrampurohit15@users.noreply.github.com>
2023-12-16 15:52:47 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TollCalculator from './components/TollCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <TollCalculator />
+    </div>
+  );
+}
+
+export default App;
